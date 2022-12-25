272bfcf0ecead6716ec0330e9514af865f56f81d
OX-S <finn.kliewer@gmail.com>
2022-12-25 01:36:09 -0500

    Initial Commit

---
+++ Shopping List/Shopping_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shopping_ListApp.swift
+//  Shopping List
+//
+//  Created by Finn Kliewer on 12/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Shopping_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
